949adc0092dd791b91a6e541a8643da42a60b815
jiangmaofang <1142548418@qq.com>
2019-08-05 09:08:12 +0800

    修改

--- fourth-learn/src/main/java/cn/com/learn/Test07.java
+++ fourth-learn/src/main/java/cn/com/learn/Test07.java
@@ -5,12 +5,13 @@ package cn.com.learn;
  * @date 2019/08/02 16:58
  */
 public class Test07 {
-  public static void main(String[] args){
-    System.out.println("11");
-    System.out.println("老家伙");
-    System.out.println("老家伙2");
-    System.out.println("老家伙3");
-    System.out.println("老家伙4");
-    System.out.println("老家伙5");
-  }
+    public static void main(String[] args){
+        System.out.println("11");
+        System.out.println("老家伙");
+        System.out.println("老家伙2");
+        System.out.println("老家伙3");
+        System.out.println("老家伙4");
+        System.out.println("老家伙5");
+        System.out.println("老家伙6");
+    }
 }
